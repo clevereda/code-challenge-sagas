f91fbde2303fe20e64195c6f6f23c628bc6a95af
Clever Mukori <cmukori@learnorteach.com>
2020-06-29 14:27:33 -0400

    router code all done

--- server/routes/zoo.router.js
+++ server/routes/zoo.router.js
@@ -5,6 +5,16 @@ const pool = require('../modules/pool.js');
 router.get('/', (req, res) => {
     // YOUR CODE HERE
     
+    const queryText =   `SELECT "species"."species_name", "class"."class_name" 
+                        FROM "species"
+                        JOIN "class" ON "species"."class_id" = "class"."id";`;
+        pool.query(queryText).then((result) => {
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error on query ${error}`);
+            res.sendStatus(500);
+        });
 });
 
 module.exports = router;
